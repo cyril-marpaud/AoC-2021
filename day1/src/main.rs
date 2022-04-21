51f888d97d35c57d7951d78da8d4191d9f21ad6d
fointard <cmarpaud@gmail.com>
2022-04-21 17:37:41 +0200

    solve d1p1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use std::{
+    self,
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+
+fn main() -> Result<()> {
+    let input = get_input("input.txt")?;
+    let first = input.iter().next().unwrap();
+
+    let count = input
+        .iter()
+        .fold((0, first), |(count, prec), curr| match prec < curr {
+            true => (count + 1, curr),
+            false => (count, curr),
+        });
+
+    println!("answer: {}", count.0);
+
+    Ok(())
+}
+
+fn get_input(filename: impl AsRef<Path>) -> Result<Vec<u32>> {
+    let file = File::open(filename).with_context(|| "Can't open file")?;
+    BufReader::new(file)
+        .lines()
+        .map(|l| {
+            l?.parse::<u32>()
+                .with_context(|| "Can't parse line into integer")
+        })
+        .collect()
 }
