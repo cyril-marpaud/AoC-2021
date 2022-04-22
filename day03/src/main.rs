ff969714d80cbfd796fb3daf1fdaf64d899ced5a
fointard <cmarpaud@gmail.com>
2022-04-22 23:00:49 +0200

    d3p1: parse input as u32 instead of String

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -6,15 +6,17 @@ use std::{
     path::Path,
 };
 
+const BIN_SIZE: usize = 12;
+
 fn main() -> Result<()> {
     let input = get_input("input.txt")?;
     let mut input = input.iter();
-    let mut ones = vec![0u32; 12];
+    let mut ones = vec![0u32; BIN_SIZE];
 
-    while let Some(s) = input.next() {
-        for (i, c) in s.chars().enumerate() {
-            if c == '1' {
-                ones[i] += 1;
+    while let Some(u) = input.next() {
+        for i in 0..BIN_SIZE {
+            if u & 1 << i != 0 {
+                ones[BIN_SIZE - 1 - i] += 1;
             }
         }
     }
@@ -25,16 +27,19 @@ fn main() -> Result<()> {
         .collect::<String>();
 
     let gamma_rate = u32::from_str_radix(&gamma_rate_string, 2)?;
-    let epsilon_rate = !gamma_rate & 0b111111111111;
+    let epsilon_rate = !gamma_rate & 2u32.pow(BIN_SIZE as u32) - 1;
 
     println!("answer: {}", gamma_rate * epsilon_rate);
     Ok(())
 }
 
-fn get_input(filename: impl AsRef<Path>) -> Result<Vec<String>> {
+fn get_input(filename: impl AsRef<Path>) -> Result<Vec<u32>> {
     let file = File::open(filename).with_context(|| "Can't open file")?;
     BufReader::new(file)
         .lines()
-        .map(|l| l.with_context(|| "Can't parse line"))
+        .map(|l| {
+            u32::from_str_radix(&l.unwrap(), 2)
+                .with_context(|| "Can't parse line into interger as binary")
+        })
         .collect()
 }
