dd9aac251a303ed922dfaa8f79a51f1adf91ba8d
fointard <cmarpaud@gmail.com>
2022-05-13 10:28:12 +0200

    d8p2: parse input into proper struct

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,10 +1,37 @@
-use anyhow::{Context, Result};
+use anyhow::{Context, Error, Result};
 use std::{
 	self,
 	fs::File,
 	io::{prelude::*, BufReader},
 	path::Path,
+	str::FromStr,
 };
+
+#[derive(Debug)]
+struct Display {
+	digits: Vec<String>,
+	output: Vec<String>,
+}
+
+impl FromStr for Display {
+	type Err = Error;
+	fn from_str(s: &str) -> Result<Self> {
+		let mut dbl_vec = s
+			.split('|')
+			.map(|half_line| {
+				half_line
+					.split_whitespace()
+					.map(|digit| digit.to_string())
+					.collect()
+			})
+			.rev() // rev so that we can pop properly
+			.collect::<Vec<Vec<String>>>();
+		Ok(Display {
+			digits: dbl_vec.pop().unwrap(),
+			output: dbl_vec.pop().unwrap(),
+		})
+	}
+}
 
 fn main() -> Result<()> {
 	let output_values = get_input("input.txt")?;
@@ -14,23 +41,9 @@ fn main() -> Result<()> {
 	Ok(())
 }
 
-fn get_input(filename: impl AsRef<Path>) -> Result<Vec<(Vec<String>, Vec<String>)>> {
+fn get_input(filename: impl AsRef<Path>) -> Result<Vec<Display>> {
 	let file = File::open(filename).with_context(|| "Can't open file")?;
 	let lines = BufReader::new(file).lines().map(Result::unwrap);
 
-	Ok(lines
-		.map(|line| {
-			let mut dbl_vec = line
-				.split('|')
-				.map(|half_line| {
-					half_line
-						.split_whitespace()
-						.map(|digit| digit.to_string())
-						.collect()
-				})
-				.rev() // rev so that we can pop properly
-				.collect::<Vec<Vec<String>>>();
-			(dbl_vec.pop().unwrap(), dbl_vec.pop().unwrap())
-		})
-		.collect())
+	Ok(lines.map(|l| l.parse().unwrap()).collect())
 }
