8d801d7047c9db2e0e05547788791981a492e061
fointard <cmarpaud@gmail.com>
2022-06-03 16:06:53 +0200

    d14p1: improve perfs by using LinkedList for template
    benchmark using vec: ~7ms
    benchmark using linkedlist: ~210µs

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,6 +1,10 @@
+#![feature(linked_list_cursors)]
+#![feature(test)]
+extern crate test;
+
 use std::{
 	self,
-	collections::{BTreeSet, HashMap, HashSet},
+	collections::{BTreeSet, HashMap, HashSet, LinkedList},
 	fs::File,
 	io::{prelude::*, BufReader},
 	path::Path,
@@ -14,7 +18,7 @@ fn main() -> Result<()> {
 	const STEP_NUMBER: usize = 10;
 	let mut sorted_counts = BTreeSet::<usize>::new();
 
-	(1..=STEP_NUMBER).for_each(|_| polymerize(&mut template, &mut insertion_rules));
+	(1..=STEP_NUMBER).for_each(|s| polymerize(&mut template, &mut insertion_rules));
 
 	for c in chars {
 		sorted_counts.insert(template.iter().filter(|el| **el == c).count());
@@ -28,21 +32,25 @@ fn main() -> Result<()> {
 	Ok(())
 }
 
-fn polymerize(template: &mut Vec<char>, insertion_rules: &mut HashMap<Vec<char>, char>) {
-	template.clone().windows(2).enumerate().rev().for_each(|w| {
-		if let Some(c) = insertion_rules.get(w.1) {
-			template.insert(w.0 + 1, *c);
+fn polymerize(template: &mut LinkedList<char>, insertion_rules: &mut HashMap<Vec<char>, char>) {
+	let mut cursor = template.cursor_front_mut();
+
+	while let Some(n) = cursor.as_cursor().peek_next() {
+		if let Some(c) = insertion_rules.get(&vec![*cursor.as_cursor().current().unwrap(), *n]) {
+			cursor.insert_after(*c);
+			cursor.move_next();
 		}
-	});
+		cursor.move_next();
+	}
 }
 
 fn get_input(
 	filename: impl AsRef<Path>,
-) -> Result<(Vec<char>, HashMap<Vec<char>, char>, HashSet<char>)> {
+) -> Result<(LinkedList<char>, HashMap<Vec<char>, char>, HashSet<char>)> {
 	let file = File::open(filename).with_context(|| "Can't open file")?;
 	let mut lines = BufReader::new(file).lines().map(Result::unwrap);
 
-	let template = lines.next().unwrap().chars().collect::<Vec<char>>();
+	let template = lines.next().unwrap().chars().collect::<LinkedList<char>>();
 
 	let mut chars = HashSet::<char>::new();
 	chars.extend(template.iter());
@@ -59,4 +67,16 @@ fn get_input(
 	});
 
 	Ok((template, insertion_rules, chars))
+}
+
+#[cfg(test)]
+mod tests {
+	use test::Bencher;
+
+	use super::main;
+
+	#[bench]
+	fn bench_main(b: &mut Bencher) {
+		b.iter(|| main());
+	}
 }
