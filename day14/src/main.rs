33c73bf230c987d5699739bdc1a2e52eca9c3835
fointard <cmarpaud@gmail.com>
2022-06-05 15:37:20 +0200

    d14p1: count pairs instead of extending string

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,10 +1,9 @@
-#![feature(linked_list_cursors)]
 #![feature(test)]
 extern crate test;
 
 use std::{
 	self,
-	collections::{BTreeSet, HashMap, HashSet, LinkedList},
+	collections::HashMap,
 	fs::File,
 	io::{prelude::*, BufReader},
 	path::Path,
@@ -12,61 +11,92 @@ use std::{
 
 use anyhow::{Context, Result};
 
+#[derive(Debug)]
+struct Template(HashMap<Vec<char>, usize>);
+
+#[derive(Debug)]
+struct Rules(HashMap<Vec<char>, Vec<Vec<char>>>);
+
 fn main() -> Result<()> {
-	let (mut template, mut insertion_rules, chars) = get_input("input.txt")?;
+	let (mut template, rules) = get_input("input.txt")?;
 
-	const STEP_NUMBER: usize = 10;
-	let mut sorted_counts = BTreeSet::<usize>::new();
+	const STEPS: usize = 10;
 
-	(1..=STEP_NUMBER).for_each(|s| polymerize(&mut template, &mut insertion_rules));
+	(1..=STEPS).for_each(|_| polymerize(&mut template, &rules));
 
-	for c in chars {
-		sorted_counts.insert(template.iter().filter(|el| **el == c).count());
-	}
+	let mut counts = HashMap::new();
+	template.0.into_iter().for_each(|(vec, size)| {
+		vec.into_iter().for_each(|c| {
+			counts
+				.entry(c)
+				.and_modify(|ref mut e| **e += size)
+				.or_insert(size);
+		});
+	});
+
+	let mut sorted_counts = Vec::from_iter(counts.values())
+		.iter()
+		.map(|&s| (s + 1) / 2)
+		.collect::<Vec<_>>();
+	sorted_counts.sort_unstable();
 
 	println!(
 		"answer: {}",
-		sorted_counts.iter().last().unwrap() - sorted_counts.iter().next().unwrap()
+		sorted_counts.iter().last().unwrap() - sorted_counts.get(0).unwrap()
 	);
 
 	Ok(())
 }
 
-fn polymerize(template: &mut LinkedList<char>, insertion_rules: &mut HashMap<Vec<char>, char>) {
-	let mut cursor = template.cursor_front_mut();
+fn polymerize(template: &mut Template, rules: &Rules) {
+	let mut temp = Template(HashMap::new());
 
-	while let Some(n) = cursor.as_cursor().peek_next() {
-		if let Some(c) = insertion_rules.get(&vec![*cursor.as_cursor().current().unwrap(), *n]) {
-			cursor.insert_after(*c);
-			cursor.move_next();
-		}
-		cursor.move_next();
-	}
+	template.0.drain().for_each(|(k, v)| {
+		rules.0.get(&k).unwrap().iter().for_each(|el| {
+			temp.0
+				.entry(el.to_vec())
+				.and_modify(|e| *e += v)
+				.or_insert(v);
+		});
+	});
+
+	*template = temp;
 }
 
-fn get_input(
-	filename: impl AsRef<Path>,
-) -> Result<(LinkedList<char>, HashMap<Vec<char>, char>, HashSet<char>)> {
+fn get_input(filename: impl AsRef<Path>) -> Result<(Template, Rules)> {
 	let file = File::open(filename).with_context(|| "Can't open file")?;
 	let mut lines = BufReader::new(file).lines().map(Result::unwrap);
 
-	let template = lines.next().unwrap().chars().collect::<LinkedList<char>>();
+	let mut template = Template(HashMap::new());
+	lines
+		.next()
+		.unwrap()
+		.chars()
+		.collect::<Vec<char>>()
+		.windows(2)
+		.for_each(|w| {
+			template
+				.0
+				.entry(w.to_vec())
+				.and_modify(|e| *e += 1)
+				.or_insert(1);
+		});
 
-	let mut chars = HashSet::<char>::new();
-	chars.extend(template.iter());
-
-	let mut insertion_rules = HashMap::new();
+	let mut rules = Rules(HashMap::new());
 	lines.skip(1).for_each(|l| {
 		let mut l = l.split(" -> ");
-		let (sequence, polymer) = (
-			l.next().unwrap().chars().collect(),
-			l.last().unwrap().chars().next().unwrap(),
-		);
-		insertion_rules.insert(sequence, polymer);
-		chars.insert(polymer);
+		let initial_pair = l.next().unwrap().chars().collect::<Vec<char>>();
+
+		let mut generated_pairs = Vec::new();
+		let mut temp = initial_pair.clone();
+		temp.insert(1, l.next().unwrap().chars().next().unwrap());
+		temp.windows(2)
+			.for_each(|p| generated_pairs.push(p.to_vec()));
+
+		rules.0.insert(initial_pair, generated_pairs);
 	});
 
-	Ok((template, insertion_rules, chars))
+	Ok((template, rules))
 }
 
 #[cfg(test)]
