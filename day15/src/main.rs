5a73cfaea8cb97f3896ff4ab5c5fe83154b6a8e3
fointard <cmarpaud@gmail.com>
2022-06-08 10:11:16 +0200

    solve d15p2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -7,37 +7,61 @@ use pathfinding::prelude::dijkstra;
 
 #[derive(Debug)]
 struct Cavern {
-	risks: [u32; Cavern::SIZE],
+	risks: [u32; Cavern::SMALL_SIZE],
+	extended_risks: Box<[u32; Cavern::BIG_SIZE]>,
 }
 
 impl Cavern {
-	const WIDTH: usize = 100;
-	const HEIGHT: usize = 100;
-	const SIZE: usize = Cavern::WIDTH * Cavern::HEIGHT;
+	const FACTOR: usize = 5;
+	const SMALL_SIDE: usize = 100;
+	const BIG_SIDE: usize = Cavern::SMALL_SIDE * Cavern::FACTOR;
+	const SMALL_SIZE: usize = Cavern::SMALL_SIDE * Cavern::SMALL_SIDE;
+	const BIG_SIZE: usize = Cavern::SMALL_SIZE * Cavern::FACTOR * Cavern::FACTOR;
 
 	fn new() -> Self {
 		Cavern {
-			risks: [0; Cavern::SIZE],
+			risks: [0; Cavern::SMALL_SIZE],
+			extended_risks: Box::new([0; Cavern::BIG_SIZE]),
 		}
 	}
 
 	fn neighbors(&self, p: usize) -> Vec<(usize, u32)> {
-		let mut neighbors = vec![];
+		let mut neighbors = Vec::new();
 
-		if p > (Cavern::WIDTH - 1) {
-			neighbors.push((p - Cavern::WIDTH, self.risks[p - Cavern::WIDTH])); // top
+		if p > (Cavern::BIG_SIDE - 1) {
+			neighbors.push((
+				p - Cavern::BIG_SIDE,
+				self.extended_risks[p - Cavern::BIG_SIDE],
+			)); // top
 		}
-		if p % Cavern::WIDTH != 0 {
-			neighbors.push((p - 1, self.risks[p - 1])); // left
+		if p % Cavern::BIG_SIDE != 0 {
+			neighbors.push((p - 1, self.extended_risks[p - 1])); // left
 		}
-		if (p + 1) % Cavern::WIDTH != 0 {
-			neighbors.push((p + 1, self.risks[p + 1])); // right
+		if (p + 1) % Cavern::BIG_SIDE != 0 {
+			neighbors.push((p + 1, self.extended_risks[p + 1])); // right
 		}
-		if p < (Cavern::SIZE - Cavern::WIDTH) {
-			neighbors.push((p + Cavern::WIDTH, self.risks[p + Cavern::WIDTH])); // bottom
+		if p < (Cavern::BIG_SIZE - Cavern::BIG_SIDE) {
+			neighbors.push((
+				p + Cavern::BIG_SIDE,
+				self.extended_risks[p + Cavern::BIG_SIDE],
+			)); // bottom
 		}
 
 		neighbors
+	}
+
+	fn extend_by_factor(&mut self) {
+		self.risks.iter().enumerate().for_each(|(p, r)| {
+			(0..Cavern::FACTOR).for_each(|x| {
+				(0..Cavern::FACTOR).for_each(|y| {
+					let position = Cavern::SMALL_SIDE * Cavern::FACTOR * (p / Cavern::SMALL_SIDE)
+						+ p % Cavern::SMALL_SIDE + Cavern::SMALL_SIDE * y
+						+ Cavern::SMALL_SIDE * Cavern::SMALL_SIDE * Cavern::FACTOR * x;
+					let new_risk = (*r - 1 + (x + y) as u32) % 9 + 1;
+					self.extended_risks[position] = new_risk;
+				})
+			});
+		});
 	}
 }
 
@@ -54,10 +78,12 @@ impl FromStr for Cavern {
 		Ok(cavern)
 	}
 }
-fn main() -> Result<()> {
-	let cavern = get_input("input.txt")?;
 
-	static GOAL: usize = Cavern::SIZE - 1;
+fn main() -> Result<()> {
+	let mut cavern = get_input("input.txt")?;
+	cavern.extend_by_factor();
+
+	static GOAL: usize = Cavern::BIG_SIZE - 1;
 	let result = dijkstra(&0, |&p| cavern.neighbors(p), |&p| p == GOAL);
 	println!("answer: {:?}", result.unwrap().1);
 
