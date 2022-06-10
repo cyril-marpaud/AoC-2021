ab38ea550c5917ace7170738d5f5dddf886aaa87
fointard <cmarpaud@gmail.com>
2022-06-10 15:19:13 +0200

    solve d16p2

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,4 +1,4 @@
-use std::{self, fs::File, io::prelude::Read, path::Path};
+use std::{self, cmp::Ordering, fs::File, io::prelude::Read, path::Path};
 
 use anyhow::{Context, Result};
 
@@ -7,7 +7,7 @@ const HEX: u32 = 16;
 
 #[derive(Debug)]
 struct Packet {
-	version: u8,
+	_version: u8,
 	type_id: PacketType,
 }
 
@@ -24,10 +24,40 @@ impl Packet {
 	const TYPEID_TYPE_LEN: usize = Packet::TYPEID_BITS_LENGTH_STR.len();
 	const VERSION_LEN: usize = 3;
 
-	fn get_version_sum(&self) -> u32 {
-		let mut sum = self.version as u32;
-		if let PacketType::Operation(subs) = &self.type_id {
-			sum += subs.iter().fold(0, |acc, s| acc + s.get_version_sum());
+	fn evaluate(&self) -> u64 {
+		match &self.type_id {
+			PacketType::Literal(v) => *v,
+			PacketType::Operation(op_type, subs) => match op_type {
+				OperationType::Sum => subs.iter().fold(0, |acc, s| acc + s.evaluate()),
+				OperationType::Product => subs.iter().fold(1, |acc, s| acc * s.evaluate()),
+				OperationType::Min => subs.iter().min_by_key(|s| s.evaluate()).unwrap().evaluate(),
+				OperationType::Max => subs.iter().max_by_key(|s| s.evaluate()).unwrap().evaluate(),
+				comp @ (OperationType::Greater | OperationType::Less | OperationType::Equal) => {
+					let mut iter = subs.iter();
+					let (pkt1, pkt2) = (
+						iter.next().unwrap().evaluate(),
+						iter.next().unwrap().evaluate(),
+					);
+					if match comp {
+						OperationType::Greater => Ordering::Greater,
+						OperationType::Less => Ordering::Less,
+						OperationType::Equal => Ordering::Equal,
+						_ => panic!(),
+					} == pkt1.cmp(&pkt2)
+					{
+						1
+					} else {
+						0
+					}
+				}
+			},
+		}
+	}
+
+	fn _get_version_sum(&self) -> u32 {
+		let mut sum = self._version as u32;
+		if let PacketType::Operation(_, subs) = &self.type_id {
+			sum += subs.iter().fold(0, |acc, s| acc + s._get_version_sum());
 		}
 		sum
 	}
@@ -83,13 +113,13 @@ impl Packet {
 				}
 				(
 					Packet {
-						version,
+						_version: version,
 						type_id: PacketType::Literal(u64::from_str_radix(&lit_buf, BIN).unwrap()),
 					},
 					pkt_len,
 				)
 			}
-			_ => {
+			op_type => {
 				let mut subs = Vec::new();
 				bits.by_ref()
 					.take(Packet::TYPEID_TYPE_LEN)
@@ -139,8 +169,20 @@ impl Packet {
 				};
 				(
 					Packet {
-						version,
-						type_id: PacketType::Operation(subs),
+						_version: version,
+						type_id: PacketType::Operation(
+							match op_type {
+								0 => OperationType::Sum,
+								1 => OperationType::Product,
+								2 => OperationType::Min,
+								3 => OperationType::Max,
+								5 => OperationType::Greater,
+								6 => OperationType::Less,
+								7 => OperationType::Equal,
+								_ => panic!(),
+							},
+							subs,
+						),
 					},
 					pkt_len,
 				)
@@ -152,7 +194,18 @@ impl Packet {
 #[derive(Debug)]
 enum PacketType {
 	Literal(u64),
-	Operation(Vec<Packet>),
+	Operation(OperationType, Vec<Packet>),
+}
+
+#[derive(Debug)]
+enum OperationType {
+	Sum,
+	Product,
+	Min,
+	Max,
+	Greater,
+	Less,
+	Equal,
 }
 
 fn main() -> Result<()> {
@@ -162,7 +215,7 @@ fn main() -> Result<()> {
 
 	let (packet, _) = Packet::parse(&mut bits);
 
-	println!("answer: {}", packet.get_version_sum());
+	println!("answer: {}", packet.evaluate());
 
 	Ok(())
 }
