49040e4e3fa34a55f62fc40b5431e71bb71f0f6b
fointard <cmarpaud@gmail.com>
2022-06-21 17:56:00 +0200

    solve d21p2

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -2,85 +2,65 @@ use std::{
 	self,
 	fs::File,
 	io::{prelude::*, BufReader},
+	ops::RangeInclusive,
 	path::Path,
 };
 
 use anyhow::{Context, Result};
 
-const MAX_SCORE: u32 = 1000;
-
-pub struct Die {
-	iter: Box<dyn Iterator<Item = u32>>,
-	rolls: u32,
-}
-
-impl Die {
-	fn new() -> Self {
-		Die {
-			iter: Box::new(1..),
-			rolls: 0,
-		}
-	}
-}
-
-impl Iterator for Die {
-	type Item = u32;
-
-	fn next(&mut self) -> Option<Self::Item> {
-		self.rolls += 1;
-		Some(self.iter.next().unwrap() % 100)
-	}
-}
+const MAX_SCORE: u8 = 21;
+const DICE_RANGE: RangeInclusive<u8> = 3..=9;
+const PROBA: [u8; 7] = [1, 3, 6, 7, 6, 3, 1]; // three three-sided dice means 27 outcomes
 
 #[derive(Clone, Debug)]
 struct Player {
-	pos: u32,
-	score: u32,
-}
-
-impl Player {
-	fn new(pos: u32) -> Self {
-		Self {
-			pos: pos - 1,
-			score: 0,
-		}
-	}
+	pos: u8,
+	score: u8,
 }
 
 fn main() -> Result<()> {
-	let (pos1, pos2) = get_input("input.txt")?;
-	let mut players = [Player::new(pos1), Player::new(pos2)];
-	let mut die = Die::new();
+	let players = get_input("input.txt")?;
 
-	let mut player_iter = (0..=1).cycle();
+	let (won_by_p2, played) = play_all(players, 0);
 
-	loop {
-		if play(&mut players[player_iter.next().unwrap()], &mut die) {
-			break;
-		}
-	}
-
-	println!(
-		"answer: {}",
-		players[player_iter.next().unwrap()].score * die.rolls
-	);
+	println!("answer: {}", won_by_p2.max(played - won_by_p2));
 
 	Ok(())
 }
 
-fn play(player: &mut Player, die: &mut Die) -> bool {
-	player.pos =
-		(player.pos + die.next().unwrap() + die.next().unwrap() + die.next().unwrap()) % 10;
-	player.score += player.pos as u32 + 1;
-
-	player.score >= MAX_SCORE // return true if win
+fn play_all(players: [Player; 2], turn: usize) -> (u64, u64) {
+	DICE_RANGE.fold((0, 0), |acc, roll| {
+		let (won_by_p2, played) = play_turn(players.clone(), turn, roll);
+		let factor = PROBA[roll as usize - 3] as u64;
+		(acc.0 + won_by_p2 * factor, acc.1 + played * factor)
+	})
 }
 
-fn get_input(filename: impl AsRef<Path>) -> Result<(u32, u32)> {
+fn play_turn(mut players: [Player; 2], turn: usize, roll: u8) -> (u64, u64) {
+	players[turn].pos = (players[turn].pos + roll) % 10;
+	players[turn].score += players[turn].pos + 1; // because positions are shifted by 1
+
+	if players[turn].score >= MAX_SCORE {
+		(turn as u64, 1) // using 'turn' to count p2 wins means less computations
+	} else {
+		play_all(players, 0usize.pow(turn as u32))
+	}
+}
+
+fn get_input(filename: impl AsRef<Path>) -> Result<[Player; 2]> {
 	let file = File::open(filename).with_context(|| "Can't open file")?;
 	let mut lines = BufReader::new(file)
 		.lines()
 		.map(|l| l.unwrap().chars().last().unwrap().to_digit(10).unwrap());
 
-	Ok((lines.next().unwrap(), lines.next().unwrap()))
+	Ok([
+		Player {
+			pos: lines.next().unwrap() as u8 - 1, // shift positions from 1-10 to 0-9 for ease
+			score: 0,
+		},
+		Player {
+			pos: lines.next().unwrap() as u8 - 1,
+			score: 0,
+		},
+	])
 }
